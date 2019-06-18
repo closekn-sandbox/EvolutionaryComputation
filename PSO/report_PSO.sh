9868399e462f0cd5947180ca0066926a7b0ebb9e
closekn <close.me.kn@gmail.com>
2019-06-18 09:53:00 +0900

    report用シェルスクリプト生成

--- PSO/report_PSO.sh
+++ PSO/report_PSO.sh
@@ -1,8 +1,8 @@
-#!/bin/zsh
+#! /bin/zsh
 
-## do
-g++ ./PSO_simulation100.cpp
-for var in 2 5 20
+g++ -o report PSO_simulation100.cpp
+
+for dim in 2 5 20
 do
-    ./a.out $var
+    ./report $dim
 done
